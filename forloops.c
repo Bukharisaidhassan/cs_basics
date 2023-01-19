bb3e72a21f5432e1cb37a4bbc7f544b9b2127de3
bukharisaidhassan <bukharisaidh@gmail.com>
2023-01-19 15:13:36 +0300

    added for loops

---
+++ forloops.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main()
+{
+    // Increment
+    for (int i = 0; i < 11; i++)
+    {
+        printf("Hello %i \n", i);
+    }
+
+    // Decrease
+    for (int i = 10; i > 1; i--)
+    {
+        printf("Hello %i \n", i);
+    }
+}
