77df4e624d0fc4864c856df1f44aa486ed3fd282
bukharisaidhassan <bukharisaidh@gmail.com>
2023-01-19 15:47:47 +0300

    first commit

---
+++ initial.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main()
+{
+
+    // increase
+    printf("first work \n");
+    for (int i = 10; i < 20; i++)
+
+    {
+        printf("Hi %i \n",i);
+    }
+
+    //   decrease
+    printf("hello loop \n");
+    for (int i = 30; i > 10; i--)
+    {
+        printf("Hi %i \n", i);
+    }
+}
