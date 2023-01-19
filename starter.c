f84105e3d1df142bfae0e290d5a46cfdbdcedfbb
bukharisaidhassan <bukharisaidh@gmail.com>
2023-01-19 15:04:19 +0300

    Starting on loops

---
+++ starter.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+
+void main(){
+    
+ int num1;
+ int num2;
+
+ printf("enter first number \n");
+ scanf("%i, &num1");
+ printf("enter second number \n");
+ scanf("&i, &num2");  
+}
