0d8a12a13044de36bcbde128617ffe852dddf370
bukharisaidhassan <bukharisaidh@gmail.com>
2023-01-19 15:17:13 +0300

    more changes on loops

--- forloops.c
+++ forloops.c
@@ -3,14 +3,16 @@
 void main()
 {
     // Increment
+    printf("First loop \n");
     for (int i = 0; i < 11; i++)
     {
         printf("Hello %i \n", i);
     }
 
     // Decrease
+    printf("Another loop \n");
     for (int i = 10; i > 1; i--)
     {
-        printf("Hello %i \n", i);
+        printf("Bye %i \n", i);
     }
 }
